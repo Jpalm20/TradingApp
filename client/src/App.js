159c72b0791f65f9ec4ddafaf1d24485ff7a4c04
Bekim Sinanovic <bekimsina98@gmail.com>
2022-08-14 14:21:12 -0400

    signup pg, routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
+
 export default function App() {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
@@ -14,9 +15,6 @@ export default function App() {
   useEffect(() => {
     dispatch(me());
   }, []);
-
-  console.log(isLoggedIn);
-  console.log(user)
 
   return (
     <Router>
