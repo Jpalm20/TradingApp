9fdd9ed5d65a9a0c01423ceffc25787997428b9f
Jonathan Palmieri <96892730+Jpalm20@users.noreply.github.com>
2023-04-02 01:07:07 -0400

    API_URL env var

--- client/src/store/trade.js
+++ client/src/store/trade.js
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const TOKEN = "";
+const API_URL = process.env.API_URL;
 
 const initialState = {
   trade: {},
@@ -23,7 +24,7 @@ export const create = createAsyncThunk(
     try {
       if (token) {
         const { user_id, trade_type, security_type, ticker_name, trade_date, expiry, strike, buy_value, units, rr, pnl, percent_wl, comments } = formInfo;
-        const res = await axios.post(`https://my-trading-app.herokuapp.com/trade/create`, {
+        const res = await axios.post(`https://API_URL.herokuapp.com/trade/create`, {
           user_id,
           trade_type,
           security_type,
@@ -60,7 +61,7 @@ export const getTrade = createAsyncThunk(
     try {
       if (token) {
         const { trade_id } = formInfo;
-        const res = await axios.get(`https://my-trading-app.herokuapp.com/trade/${trade_id}`,{
+        const res = await axios.get(`https://API_URL.herokuapp.com/trade/${trade_id}`,{
           headers: {
             Authorization: "Bearer " + token,
           }
@@ -83,7 +84,7 @@ export const deleteTrade = createAsyncThunk(
     try {
       if (token) {
         const { trade_id } = formInfo;
-        const res = await axios.delete(`https://my-trading-app.herokuapp.com/trade/${trade_id}`,{
+        const res = await axios.delete(`https://API_URL.herokuapp.com/trade/${trade_id}`,{
           headers: {
             Authorization: "Bearer " + token,
           }
@@ -106,7 +107,7 @@ export const update = createAsyncThunk(
     try {
       if (token) {
         const { trade_id, user_id, trade_type, security_type, ticker_name, trade_date, expiry, strike, buy_value, units, rr, pnl, percent_wl, comments } = formInfo;
-        const res = await axios.post(`https://my-trading-app.herokuapp.com/trade/${trade_id}`, {
+        const res = await axios.post(`https://API_URL.herokuapp.com/trade/${trade_id}`, {
           user_id,
           trade_type,
           security_type,
